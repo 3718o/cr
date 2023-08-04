d233fa998b709cb59ab8294f9ae6f8609e0f396f
Random Account <127893035+3718o@users.noreply.github.com>
2023-08-03 20:42:59 -0400

    Update chatroom.js

--- chatroom.js
+++ chatroom.js
@@ -1,29 +1,39 @@
-            $(document).ready(function () {
-              cometApi.start({node: "app.comet-server.ru", dev_id: 15})
+$(document).ready(function() {
+  cometApi.start({ node: "app.comet-server.ru", dev_id: 15 })
 
-              cometApi.subscription("simplechat.newMessage", function (event) {
-                $("#web_chat").append('<b>' + HtmlEncode(event.data.name) + '</b>')
-                $("#web_chat").append('<pre>' + HtmlEncode(event.data.text) + '</pre>')
-                $("#web_chat").append('<br>')
-              })
-            })
+  cometApi.subscription("simplechat.newMessage", function(event) {
+    $("#web_chat").append('<b>' + HtmlEncode(event.data.name + (((new Date().getHours() - 1) % 12) + 1) + ":" + ((new Date().getMinutes()) < 10 ? "0" : "") + new Date().getMinutes() + ":" + ((new Date().getSeconds()) < 10 ? "0" : "") + new Date().getSeconds()) + ((new Date().getHours()) > 11 ? " PM" : " AM") + '</b>')
+    $("#web_chat").append('<pre>' + HtmlEncode(event.data.text) + '</pre>')
+    $("#web_chat").append('<br>')
+  })
+})
 
-            function HtmlEncode(s) {
-              var el = document.createElement("div");
-              el.innerText = el.textContent = s;
-              s = el.innerHTML;
-              return s;
-            }
-            function send() {
-              var name = $('#n').val();
-              var text = $('#t').val();
-               
+function HtmlEncode(s) {
+  var el = document.createElement("div");
+  el.innerText = el.textContent = s;
+  s = el.innerHTML;
+  return s;
+}
 
-              $.ajax({
-                url: "https://comet-server.com/doc/CppComet/chat-example/chat.php",
-                type: "POST",
-               data: "text=" + encodeURIComponent(text) + "&name=" + " 👤 " + encodeURIComponent(name) + " - " + new Date().toLocaleTimeString()
-              });
-              document.getElementById("t").value = "";
-            }
-            
+
+
+
+document.body.onkeydown = function(event){
+   event = event || window.event;
+    var keycode = event.charCode || event.keyCode;
+    if(keycode === 13){
+        send();
+    }
+}
+
+function send() {
+  var name = $('#n').val(); 
+  var text = $('#t').val();
+
+  $.ajax({
+    url: "https://comet-server.com/doc/CppComet/chat-example/chat.php",
+    type: "POST",
+    data: "text=" + encodeURIComponent(text) + "&name=" + " 👤 " + encodeURIComponent(name) + " - " 
+  });
+  document.getElementById("t").value = "";
+}
